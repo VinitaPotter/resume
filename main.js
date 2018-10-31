67bd6fc14c5d3bc729cddab90ab9906c1ba1f28b
Vinita <vinitavkumbhar@gmail.com>
2018-11-01 00:23:39 +0530

    sticky navbar, mobile optimization

---
+++ main.js
@@ -0,0 +1,16 @@
+//new WOW().init();
+
+window.onscroll = function() {stickyNav()};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
